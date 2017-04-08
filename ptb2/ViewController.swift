4b4e0f7e56382277dde41e714616bb252f2d5e2a
PhongLe <phongle@PhongLes-MacBook-Pro.local>
2017-04-08 12:52:21 +0700

    Initial Commit

---
+++ ptb2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ptb2
+//
+//  Created by PhongLe on 4/8/17.
+//  Copyright © 2017 PhongLe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
